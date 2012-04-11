a5a778858f8e89450bffe3468a34493beb72b42c
Jenkins CI <jenkins@ci.angularjs.org>
2012-04-10 20:56:39 -0700

    improve opera script

--- bin/opera.sh
+++ bin/opera.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
-trap "{ killall Opera;  rm -rf ~/Library/Opera/; exit 0; }" EXIT
-/Applications/Opera.app/Contents/MacOS/Opera $1
+
+PROFILE_DIR=/Users/jenkins/tmp/opera-$RANDOM
+LOG_FILE=/Users/jenkins/bin/opera.log
+
+cleanup() {
+  killall Opera
+  rm -rf $PROFILE_DIR
+}
+
+# register a trap
+trap "{ cleanup; exit 0; }" EXIT
+
+
+# start
+echo -e "\n\n\n----------------+ `date` +----------------\n" >> $LOG_FILE
+
+/Applications/Opera.app/Contents/MacOS/Opera \
+  -personaldir $PROFILE_DIR \
+  -nomail \
+  $1 \
+  >> $LOG_FILE 2>&1
