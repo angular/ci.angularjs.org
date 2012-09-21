e402d8ab81a74a8619a924e499cb655484b4416d
Jenkins CI <jenkins@ci.angularjs.org>
2012-09-21 14:36:00 -0700

    firefox.sh - create ff profile before launching

--- bin/firefox.sh
+++ bin/firefox.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# cleanup
-killall -9 firefox
-rm -rf ~/Library/Application\ Support/Firefox/Profiles/w9fd2jwf.default/sessionstore.*
-rm -f ~/Library/Application\ Support/Firefox/Profiles/w9fd2jwf.default/.parentlock
+PROFILE_DIR=/Users/jenkins/tmp/firefox-$RANDOM
+LOG_FILE=/Users/jenkins/bin/firefox.log
+
+cleanup() {
+  killall "firefox"
+  rm -rf $PROFILE_DIR
+}
 
 # register a trap
-trap "{ killall firefox; exit 0; }" EXIT
+trap "cleanup; exit 0" EXIT
 
-/Applications/Firefox.app/Contents/MacOS/firefox $*
+
+# start
+echo -e "\n\n\n----------------+ `date` +----------------\n" >> $LOG_FILE
+
+/Applications/Firefox.app/Contents/MacOS/firefox -CreateProfile "test $PROFILE_DIR"
+
+/Applications/Firefox.app/Contents/MacOS/firefox \
+  -profile $PROFILE_DIR \
+  $1 \
+  >> $LOG_FILE 2>&1
+
