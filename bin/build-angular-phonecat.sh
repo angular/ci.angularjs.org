77ea1aa6b773925bf4c95aa234c609baec68a94c
jenkins <ci-bot@angularjs.org>
2013-02-08 17:09:51 -0800

    add build-angular-* scripts

---
+++ bin/build-angular-phonecat.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Enable tracing and exit on first failure
+set -xe
+
+
+# Define reasonable set of browsers in case we are running manually from commandline
+if [[ -z "$BROWSERS" ]]
+then
+  BROWSERS="Chrome,Firefox,Opera,/Users/jenkins/bin/safari.sh,/Users/jenkins/bin/ie8.sh,/Users/jenkins/bin/ie9.sh"
+fi
+
+if [[ -z "$BROWSERS_E2E" ]]
+then
+  BROWSERS_E2E="Chrome,Firefox,/Users/jenkins/bin/safari.sh"
+fi
+
+
+./scripts/web-server.js > /dev/null &
+WEBSERVER_PID=$!
+
+trap "{ kill $WEBSERVER_PID; exit; }" EXIT
+
+for i in {3..11}; do
+
+  STEP="step-$i"
+
+  git checkout -f $STEP
+
+  ./scripts/test.sh \
+      --browsers="$BROWSERS" \
+      --reporters=dots \
+      --single-run \
+      --no-colors
+
+  ./scripts/e2e-test.sh \
+      --browsers="$BROWSERS_E2E" \
+      --reporters=dots \
+      --no-colors
+
+done
---
+++ bin/build-angular-seed.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Enable tracing and exit on first failure
+set -xe
+
+
+# Define reasonable set of browsers in case we are running manually from commandline
+if [[ -z "$BROWSERS" ]]
+then
+  BROWSERS="Chrome,Firefox,Opera,/Users/jenkins/bin/safari.sh,/Users/jenkins/bin/ie8.sh,/Users/jenkins/bin/ie9.sh"
+fi
+
+
+rm -rf test_out
+
+./scripts/test.sh \
+  --single-run \
+  --browsers="$BROWSERS" \
+  --reporters="dots,junit" \
+  --no-colors
+
+
+./scripts/web-server.js > /dev/null &
+WEBSERVER_PID=$!
+
+trap "{ kill $WEBSERVER_PID; exit; }" EXIT
+
+./scripts/e2e-test.sh \
+  --single-run \
+  --browsers="$BROWSERS" \
+  --reporters="dots,junit" \
+  --no-colors
