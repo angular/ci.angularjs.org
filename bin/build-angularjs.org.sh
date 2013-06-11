c4f907c1cd3a138497e8fc68515955f4a80fbef0
Jeff Cross <middlefloor@gmail.com>
2013-06-11 09:30:16 -0700

    fix(bower): Fixed bower install command in script.

--- bin/build-angularjs.org.sh
+++ bin/build-angularjs.org.sh
@@ -4,8 +4,7 @@
 set -xe
 
 # BUILD #
-bower install --save https://selenium.googlecode.com/files/selenium-server-standalone-2.33.0.jar
-bower install --save https://chromedriver.googlecode.com/files/chromedriver_mac_26.0.1383.0.zip
+bower install
 npm install --color false
 grunt configure --target=prod
 
