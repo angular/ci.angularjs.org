fbed91f504bbaa1eb7e307bdf81daa87aefa7cbe
Jeff Cross <middlefloor@gmail.com>
2013-06-20 10:09:37 -0700

    feat(IE10): Added IE10 boot to bin.

---
+++ bin/ie10.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+VMNAME="Windows 7 (IE10)"
+VMPATH="/Users/jenkins/Documents/Virtual Machines.localized/Windows 7 (IE10).vmwarevm/Windows 7 (IE10).vmx"
+LOG_FILE="/Users/jenkins/bin/ie10.log"
+
+. ie_.sh
