30064091ba35d3b2b8e6f9a8854c48ee02fe5b5d
Jeff Cross <middlefloor@gmail.com>
2013-08-15 11:18:44 -0700

    fix(build-angularjs.org): restored chmod of selenium chromedriver.

--- bin/build-angularjs.org.sh
+++ bin/build-angularjs.org.sh
@@ -7,6 +7,7 @@ set -xe
 npm install --color false
 grunt configure --target=prod
 bower install --production
+chmod 777 `pwd`/components/chromedriver_mac_26.0.1383.0/chromedrive
 
 # E2E TESTS #
 grunt ht2j
