0ac3178f99c789d380e44eca89439a2dc15f5863
Jeff Cross <middlefloor@gmail.com>
2013-08-15 11:29:47 -0700

    fix(build-angularjs.org): Added missing 'r' to end of line.

--- bin/build-angularjs.org.sh
+++ bin/build-angularjs.org.sh
@@ -7,7 +7,7 @@ set -xe
 npm install --color false
 grunt configure --target=prod
 bower install --production
-chmod 777 `pwd`/components/chromedriver_mac_26.0.1383.0/chromedrive
+chmod 777 `pwd`/components/chromedriver_mac_26.0.1383.0/chromedriver
 
 # E2E TESTS #
 grunt ht2j
