792948483f9f10da65c4e1720210cb9c654109c8
Jeff Cross <middlefloor@gmail.com>
2013-08-20 14:11:33 -0700

    feat(angularjs.org): Updated testing process.

--- bin/build-angularjs.org.sh
+++ bin/build-angularjs.org.sh
@@ -6,10 +6,8 @@ set -xe
 # BUILD #
 npm install --color false
 grunt configure --target=prod
-bower install --production
-chmod 777 `pwd`/components/chromedriver_mac_26.0.1383.0/chromedriver
+grunt install-selenium
 
 # E2E TESTS #
 grunt ht2j
-./node_modules/.bin/protractor protractorConf.js
-
+grunt test
