af9fecb9492122e7ef340993875139407a19e991
Igor Minar <igor@angularjs.org>
2013-11-26 11:22:01 -0800

    ie11.sh - fix bad copy\&pase

--- bin/ie11.sh
+++ bin/ie11.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 VMNAME="Windows 7 (IE11)"
-VMPATH="/Users/jenkins/Virtual Machines/Windows 7 (IE11).vmwarevm/Windows 7 (IE
+VMPATH="/Users/jenkins/Virtual Machines/Windows 7 (IE11).vmwarevm/Windows 7 (IE11).vmx"
 LOG_FILE="/Users/jenkins/bin/ie11.log"
 
 . ie_.sh
